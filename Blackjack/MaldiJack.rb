ec3c18e2d8aa97cc87fb4bdb699c33eb1990df60
Milind Patil <mailmaldi@gmail.com>
2013-03-23 12:35:59 -0400

    maldijack with value redefined

--- Blackjack/MaldiJack.rb
+++ Blackjack/MaldiJack.rb
@@ -2,11 +2,12 @@ SUITE = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
 # simply *4 to get 1 full deck and *4*n to get n decks and then .shuffle a few times
 
 class Hand
-  attr_accessor :cards, :bet , :is_playing
-  def initialize(cards,bet)
+  attr_accessor :cards, :bet , :is_playing, :hits
+  def initialize(bet,cards)
     @is_playing=true
     @bet = bet
-    @cards= cards
+    @cards = cards
+    @hits = 0
   end
 
   def print_hand()
@@ -19,37 +20,42 @@ class Hand
     return (value() == 21)
   end
 
-  # Gets the value of the hand for the player according to the player cards
+  # Gets the value of the hand
   def value()
     sum = 0
-    # Partition the array by string and integer then reverse to put aces at back
-    @cards.partition{|x| x.is_a? String}.map(&:sort).flatten.reverse_each do |i|
+    numAces = 0
+    @cards.each do |i|
       if ["Q", "J", "K"].include?(i)
         sum += 10
       elsif i == "A"
-        if sum + 11 > 21
-          sum += 1
-        else
-          sum += 11
-        end
+        numAces += 1
       else
         sum += i
       end
-    end
+    end # end cards.each
+    #now we have the hard sum, and number of aces, find the closest sum to 21
+    if numAces > 0
+      tempSum1 = 21 - (sum + 11 + numAces -1)
+      tempSum2 = 21 - (sum + numAces)
+      if tempSum1 < 0
+        sum = sum + numAces
+      else
+        sum = sum + 11 + numAces -1
+      end
+    end # end numAces>0
     return sum
-  end
+  end #end value()
 
-end
+end #end class Hand
 
 class Player
   attr_accessor :hands, :amount , :is_playing,
-  def initialize(amount, player_number)
+  def initialize(cards, bet,amount, player_number)
     @amount = amount
     @is_playing = true
-    @bet = 0
     @player_number = player_number
     @hands= Array.new
-    @hands[0] = Hands.new()
+    @hands[0] = Hand.new(bet,cards)
   end
 
   def reset()
